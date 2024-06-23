542c3faec9fbecc58e5464e2ad20c0436d57acb9
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-06-23 21:43:08 +0300

    updating the web font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["outfit"] });
 
 export const metadata = {
   title: "highPost",
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={outfit.className}>{children}</body>
     </html>
   );
 }
